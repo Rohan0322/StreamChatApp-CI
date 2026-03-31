b2b039d15abba0a89a95948582cc7be99968d559
Ubuntu <azureuser@vm-devops-agent.lq3dvzb4bcjutegsnhiwa5xm5d.tx.internal.cloudapp.net>
2026-03-31 18:42:46 +0000

    Final changes

--- examples/SampleApp/babel.config.js
+++ examples/SampleApp/babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['babel-plugin-react-compiler', 'react-native-worklets/plugin'],
+  plugins: [
+    'babel-plugin-react-compiler',
+    // must be last
+    'react-native-reanimated/plugin',
+  ],
 };
--- package/native-package/android/src/main/java/com/streamchatreactnative/StreamChatReactNativePackage.java
+++ package/native-package/android/src/main/java/com/streamchatreactnative/StreamChatReactNativePackage.java
@@ -1,15 +1,15 @@
 package com.streamchatreactnative;
 
 import androidx.annotation.Nullable;
+import com.facebook.react.TurboReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.module.model.ReactModuleInfo;
 import com.facebook.react.module.model.ReactModuleInfoProvider;
-import com.facebook.react.TurboReactPackage;
 import com.facebook.react.uimanager.ViewManager;
 
-import java.util.HashMap;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -19,9 +19,9 @@ public class StreamChatReactNativePackage extends TurboReactPackage {
   @Override
   public NativeModule getModule(String name, ReactApplicationContext reactContext) {
     if (name.equals(StreamChatReactNativeModule.NAME)) {
-        return new StreamChatReactNativeModule(reactContext);
+      return new StreamChatReactNativeModule(reactContext);
     } else {
-        return null;
+      return null;
     }
   }
 
@@ -31,22 +31,39 @@ public class StreamChatReactNativePackage extends TurboReactPackage {
       final Map<String, ReactModuleInfo> moduleInfos = new HashMap<>();
       boolean isTurboModule = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
       moduleInfos.put(
-              StreamChatReactNativeModule.NAME,
-              new ReactModuleInfo(
-                      StreamChatReactNativeModule.NAME,
-                      StreamChatReactNativeModule.NAME,
-                      false, // canOverrideExistingModule
-                      false, // needsEagerInit
-                      true, // hasConstants
-                      false, // isCxxModule
-                      isTurboModule // isTurboModule
-      ));
+        StreamChatReactNativeModule.NAME,
+        new ReactModuleInfo(
+          StreamChatReactNativeModule.NAME,
+          StreamChatReactNativeModule.NAME,
+          false, // canOverrideExistingModule
+          false, // needsEagerInit
+          true, // hasConstants
+          false, // isCxxModule
+          isTurboModule // isTurboModule
+        )
+      );
       return moduleInfos;
     };
   }
 
   @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-    return Collections.<ViewManager>singletonList(new StreamShimmerViewManager());
+    // StreamShimmerViewManager only exists/compiles with New Architecture.
+    // We must avoid referencing it directly, otherwise javac fails even if the branch is never executed.
+    if (!BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      return Collections.emptyList();
+    }
+
+    try {
+      Class<?> clazz =
+        Class.forName("com.streamchatreactnative.shared.StreamShimmerViewManager");
+      Object instance = clazz.getDeclaredConstructor().newInstance();
+      @SuppressWarnings("unchecked")
+      ViewManager viewManager = (ViewManager) instance;
+      return Collections.<ViewManager>singletonList(viewManager);
+    } catch (Throwable t) {
+      // If something goes wrong (class missing, reflection blocked, etc.), don't crash the build/app.
+      return Collections.emptyList();
+    }
   }
 }
